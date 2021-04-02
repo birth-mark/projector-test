9add35da8937fe268ff1f8e72ea2132b3ed105fc
birth-mark <lera.slyun@gmail.com>
2021-04-02 22:11:32 +0300

    added first node server

---
+++ node-server/index.js
@@ -0,0 +1,36 @@
+
+const http = require('http');
+const url = require('url');
+
+const port = process.argv[2] || 6060
+
+const hostname = '127.0.0.1';
+
+const mapHost = {
+  '/hello' : (name) =>  {
+    if(name) return `Hello, ${name}`;
+    return 'Hello, world';
+  },
+  '/goodbye': (name) => {
+    if(name) return `Goodbye, ${name}`;
+    return 'Goodbye';
+  }
+}
+
+const server = http.createServer((req, res) => {
+  const myURL = url.parse(req.url, true);
+  let answer = mapHost[myURL.pathname];
+
+  if(!answer){
+    res.statusCode = 404;
+    res.end();
+  }
+
+  res.statusCode = 200;
+  res.setHeader('Contetn-Type', 'text/plain');
+  res.end(answer(myURL.query.name));
+})
+
+server.listen(port, hostname, ()=>{
+  console.log(`server running at http://${hostname}:${port}`)
+})
